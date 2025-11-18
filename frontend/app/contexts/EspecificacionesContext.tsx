58524215876332aad567938f8c245fe3cf4d9960
samunv <sur00044@gmail.com>
2025-11-18 01:27:15 +0100

    EspecificacionesContext

---
+++ frontend/app/contexts/EspecificacionesContext.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { createContext, ReactNode, useCallback, useContext, useState } from "react";
+import { Especificaciones } from '../interfaces/Especificaciones';
+
+interface EspecificacionesContextType {
+  especificaciones: Especificaciones | null;
+  setEspecificaciones: (esp: Especificaciones) => void;
+  updateEspecificaciones: (
+    claveActualizar: string,
+    valorActualizar: string
+  ) => void;
+}
+
+const EspecificacionesContext =
+  createContext<EspecificacionesContextType | undefined>(undefined);
+
+export const EspecificacionesProvider = ({children}:{
+  children: ReactNode;
+})=>{
+
+  const [especificaciones, setEspecificaciones] = useState<Especificaciones | null>(null)
+
+  const updateEspecificaciones = useCallback(
+      (claveActualizar: string, valorActualizar: string) => {
+        setEspecificaciones((espPrevias) => {
+          if (!espPrevias) {
+            return {
+              [claveActualizar]: valorActualizar,
+            };
+          }
+          return {
+            ...espPrevias,
+            [claveActualizar]: valorActualizar,
+          };
+        });
+      },
+      []
+    );
+
+  return (
+    <EspecificacionesContext.Provider value={{especificaciones, setEspecificaciones, updateEspecificaciones}}>
+
+    </EspecificacionesContext.Provider>
+  )
+}
+export const useEspecificaciones = ()=>{
+  const context = useContext(EspecificacionesContext);
+    if (context === undefined) {
+      throw new Error(
+        "Error"
+      );
+    }
+    return context;
+}
