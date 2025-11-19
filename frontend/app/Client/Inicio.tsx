2326c3493982c507a7193b63be731bbccdcdfa52
samunv <sur00044@gmail.com>
2025-11-19 03:13:39 +0100

    Detalles y correcciones css

--- frontend/app/Client/Inicio.tsx
+++ frontend/app/Client/Inicio.tsx
@@ -101,12 +101,12 @@ export default function Inicio() {
     });
   }
 
-  function obtenerTipoImagenDeImagen(imagenBase64URL: string){
+  function obtenerTipoImagenDeImagen(imagenBase64URL: string) {
     const match = imagenBase64URL.match(/^data:([^;]+);base64,/);
-  
-  if (match && match[1]) {
-    updateSolicitudRecetaCallback("tipoImagen", match[1])
-  }
+
+    if (match && match[1]) {
+      updateSolicitudRecetaCallback("tipoImagen", match[1]);
+    }
   }
 
   // useEffect(() => {
@@ -193,12 +193,45 @@ export default function Inicio() {
         </svg>
       </div>
 
-      <h1 className="text-[#343A40] text-3xl text-center font-medium">
-        ¡Hola! soy tu chef de recetas,
-      </h1>
+      {!respuestaIA && (
+        <h1 className="text-[#343A40] text-3xl text-center font-medium">
+          ¡Hola! soy tu chef de recetas,
+        </h1>
+      )}
+
+      {respuestaIA && (
+        <div className="w-[700px] bg-white p-8 rounded-3xl shadow-xl border border-[#E67E22]/20 animate-fadeIn mb-10">
+          <div className="flex items-center gap-3 mb-6 border-b border-gray-100 pb-4">
+            <div className="bg-orange-100 p-2 rounded-full text-[#E67E22]">
+              <svg
+                viewBox="0 0 24 24"
+                fill="none"
+                width={24}
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M7 5C4.23858 5 2 7.23858 2 10C2 12.0503 3.2341 13.8124 5 14.584V17.25H19L19 14.584C20.7659 13.8124 22 12.0503 22 10C22 7.23858 19.7614 5 17 5C16.7495 5 16.5033 5.01842 16.2626 5.05399C15.6604 3.27806 13.9794 2 12 2C10.0206 2 8.33961 3.27806 7.73736 5.05399C7.49673 5.01842 7.25052 5 7 5Z"
+                  className="fill-orange-500"
+                ></path>
+                <path
+                  d="M18.9983 18.75H5.00169C5.01188 20.1469 5.08343 20.9119 5.58579 21.4142C6.17157 22 7.11438 22 9 22H15C16.8856 22 17.8284 22 18.4142 21.4142C18.9166 20.9119 18.9881 20.1469 18.9983 18.75Z"
+                  className="fill-orange-500"
+                ></path>
+              </svg>
+            </div>
+            <h2 className="text-2xl font-bold text-[#343A40]">ChefGPT</h2>
+          </div>
+
+          <div className="prose prose-orange max-w-none text-[#343A40]  leading-relaxed">
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>
+              {respuestaIA}
+            </ReactMarkdown>
+          </div>
+        </div>
+      )}
 
       {/* BARRA DE ENTRADA PRINCIPAL */}
-      <div className="bg-[#FDFBF5] text-[#343A40] p-3 border-2 border-[#8D6E63]/30 rounded-2xl flex flex-row items-center w-[700px] gap-3 shadow-lg focus-within:border-[#E67E22] focus-within:ring-2 focus-within:ring-[#E67E22]/30 transition-all duration-300 min-h-[65px] z-20">
+      <div className="bg-[#FDFBF5] text-[#343A40] p-3 border-2 border-[#8D6E63]/30 rounded-2xl flex flex-row items-center w-[700px] gap-3 shadow-lg focus-within:border-[#E67E22] focus-within:ring-2 focus-within:ring-[#E67E22]/30 transition-all duration-300 min-h-[65px] z-20" >
         {/* Dropdown de Modelos */}
         <div className="relative" ref={dropdownRef}>
           <button
@@ -351,40 +384,6 @@ export default function Inicio() {
       >
         <IoIosAddCircle size={20} />
       </BotonGeneral>
-
-      {/* --- ZONA DE RESPUESTA (NUEVO) --- */}
-      {respuestaIA && (
-        <div className="w-[700px] bg-white p-8 rounded-3xl shadow-xl border border-[#E67E22]/20 animate-fadeIn mb-10">
-          <div className="flex items-center gap-3 mb-6 border-b border-gray-100 pb-4">
-            <div className="bg-orange-100 p-2 rounded-full text-[#E67E22]">
-              <svg
-                viewBox="0 0 24 24"
-                fill="none"
-                width={24}
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M7 5C4.23858 5 2 7.23858 2 10C2 12.0503 3.2341 13.8124 5 14.584V17.25H19L19 14.584C20.7659 13.8124 22 12.0503 22 10C22 7.23858 19.7614 5 17 5C16.7495 5 16.5033 5.01842 16.2626 5.05399C15.6604 3.27806 13.9794 2 12 2C10.0206 2 8.33961 3.27806 7.73736 5.05399C7.49673 5.01842 7.25052 5 7 5Z"
-                  className="fill-orange-500"
-                ></path>
-                <path
-                  d="M18.9983 18.75H5.00169C5.01188 20.1469 5.08343 20.9119 5.58579 21.4142C6.17157 22 7.11438 22 9 22H15C16.8856 22 17.8284 22 18.4142 21.4142C18.9166 20.9119 18.9881 20.1469 18.9983 18.75Z"
-                  className="fill-orange-500"
-                ></path>
-              </svg>
-            </div>
-            <h2 className="text-2xl font-bold text-[#343A40]">
-              ChefGPT
-            </h2>
-          </div>
-
-          <div className="prose prose-orange max-w-none text-[#343A40]  leading-relaxed">
-            <ReactMarkdown remarkPlugins={[remarkGfm]}>
-  {respuestaIA}
-</ReactMarkdown>
-          </div>
-        </div>
-      )}
 
       {mostrarFormEspecificaciones && (
         <FormularioEspecificaciones cerrar={cerrarFormulario} />
