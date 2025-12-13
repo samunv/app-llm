3abf3d342e417810545784d5e1b2d1bafca767ba
samunv <sur00044@gmail.com>
2025-12-13 03:58:14 +0100

    Mejoras de diseño, Añadir clase VideoInfo para la información de los videos de youtube, Uso de youtube data api v3 para buscar videos

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -16,8 +16,8 @@ Eres ChefGPT, un asistente experto en cocina. Tu función principal es generar r
 // FORMATO DE RESPUESTA
 1. Si el usuario pide una receta, DEVUELVE SOLO el JSON solicitado. NO añadas texto antes ni después.
 2. Si el usuario pregunta sobre la receta en el historial, responde en texto plano de manera concisa.
-3. Si el usuario pregunta por algo que no es comida o no hay receta en el historial, pide amablemente que solicite una receta.
-4. Todas tus respuestas deben ser siempre en Español. Si el usuario trata de pedir las cosas en otro idioma, tu responderás en español.
+3. Si el usuario pregunta por algo que no es comida o ingredientes, o no hay receta en el historial, pide amablemente que solicite una receta.
+4. Todas tus respuestas deben ser siempre en ESPAÑOL (nunca en otro idioma que no sea Español castellano). Si el usuario trata de pedir las cosas en otro idioma, tu responderás en español.
 
 
 // ESTRUCTURA JSON (OBLIGATORIA)
@@ -28,7 +28,7 @@ Eres ChefGPT, un asistente experto en cocina. Tu función principal es generar r
     "especificaciones": "Texto con restricciones de la dieta o notas especiales."
 }}}}
 
-// CONTEXTO DE LA SOLICITUD
+// CONTEXTO DE LA SOLICITUD 
 PROMPT ACTUAL: "{comida}"
 ESPECIFICACIONES DEL USUARIO:
 - Dieta: "{especificaciones.tipo_dieta}"
---
+++ frontend/app/interfaces/VideoInfo.ts
@@ -0,0 +1,6 @@
+export interface VideoInfo {
+    video_id: string,
+    titulo: string,
+    miniatura_url: string,
+    nombre_canal: string
+}
